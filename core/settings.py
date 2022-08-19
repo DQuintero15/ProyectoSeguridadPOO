6ea52c88385c70b59952283838137c766daeaac2
Daniel Quintero <quinterod164@gmail.com>
2022-08-19 08:53:16 -0500

    Inclusion de apps en settings

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "AppSeguridad.apps.AppseguridadConfig",
+    "landing.apps.LandingConfig",
+    "login.apps.LoginConfig",
+    "register.apps.RegisterConfig"
 ]
 
 MIDDLEWARE = [
