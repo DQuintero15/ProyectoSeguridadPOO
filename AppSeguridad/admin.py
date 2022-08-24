5f29495744e92929165a76c992c6801a75a1d3d7
Daniel Quintero <quinterod164@gmail.com>
2022-08-24 18:54:12 -0500

    Actualización de base de datos ORM

--- AppSeguridad/admin.py
+++ AppSeguridad/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Arma
 from .models import Protegido
+from .models import Escolta
 
 admin.site.register(Protegido)
 admin.site.register(Arma)
+admin.site.register(Escolta)
--- AppSeguridad/models.py
+++ AppSeguridad/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 
 class DatosPersonales(models.Model):
-
     A_POSITIVO = "A+"
     A_NEGATIV0 = "A-"
     B_POSITIVO = "B+"
@@ -31,7 +27,7 @@ class DatosPersonales(models.Model):
     edad = models.IntegerField(default=18)
     correo_electronico = models.EmailField(max_length=30, blank=True)
     tipo_sangre = models.CharField(
-        max_length=3, choices=TIPOS_SANGRE, default=TIPOS_SANGRE[0]
+        max_length=3, choices=TIPOS_SANGRE, default=TIPOS_SANGRE[6]
     )
     numero_cedula = models.CharField(max_length=15)
     copia_cedula = models.FileField(upload_to="archivos\\cedulas")
@@ -55,9 +51,36 @@ class Protegido(DatosPersonales):
 
 
 class Arma(models.Model):
-    id_arma = models.AutoField(primary_key=True)
+    serial = models.CharField(max_length=40, default="Sin registrar", primary_key=True)
     nombre = models.CharField(max_length=100)
     calibre = models.CharField(max_length=10)
-    serial = models.CharField(max_length=30, default="Sin registrar")
-    observacion = models.TextField(max_length=230, blank=True)
+    observacion = models.TextField(max_length=230, blank=True, default="Sin novedad")
     letal = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre + " " + self.calibre
+
+
+class Escolta(DatosPersonales):
+    arma = models.OneToOneField(Arma, on_delete=models.CASCADE)
+    id_escolta = models.AutoField(primary_key=True)
+
+
+class Posicion(models.Model):
+    id_posicion = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, unique=True)
+    piso = models.IntegerField()
+
+
+class Instalacion(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, primary_key=True)
+    ubicacion_Latitud = models.DecimalField(max_digits=15, decimal_places=2)
+    ubicacion_longitud = models.DecimalField(max_digits=15, decimal_places=2)
+    numero_plantas = models.IntegerField(default=1, blank=True)
+    direccion = models.CharField(max_length=30, blank=True)
+    descipcion = models.CharField(max_length=100, blank=True)
+    planos = models.FileField(upload_to="archivos\\planos", blank=True)
+
+
+class EsquemaProteccionInstalacion(models.Model):
+    id_esquema_proteccion = models.AutoField(primary_key=True)
