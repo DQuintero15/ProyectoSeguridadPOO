90e9237244acfece9269fe2793b944ceaf39aa60
Daniel Quintero <quinterod164@gmail.com>
2022-09-08 15:13:50 -0500

    Descarte de modelos antiguos

--- AppSeguridad/admin.py
+++ AppSeguridad/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import Arma
-from .models import Protegido
-from .models import Escolta
 
-admin.site.register(Protegido)
 admin.site.register(Arma)
-admin.site.register(Escolta)
