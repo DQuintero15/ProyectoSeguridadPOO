4f60b619d63013784cdeb1b421aa2c24fbc97997
ErickBCruz <erickbaezcruz@gmail.com>
2022-09-14 13:40:03 -0500

    Cambio de urls

--- core/urls.py
+++ core/urls.py
@@ -21,16 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path("", include("landing.urls")),
     path("login/", include("user.urls")),
-=======
-    path("home/", include("landing.urls")),
->>>>>>> eda3c2b24b79ba3e8bef66f30288c5f82274110f
-=======
-    path("home/", include("landing.urls")),
->>>>>>> f01ceb22af857cfca7d28c500f7358420e23879e
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
