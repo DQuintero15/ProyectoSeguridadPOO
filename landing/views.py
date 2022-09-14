15fb5677d8aaab4db54848beea327d78ae780200
ErickBCruz <erickbaezcruz@gmail.com>
2022-09-14 14:49:21 -0500

    Ajuste de iews

--- landing/views.py
+++ landing/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 from FuerzasMilitares.models import FuerzasMilitares
-<<<<<<< HEAD
-
-
-def home(request):  
-    return render(request, "home.html")
-
-=======
 from django.shortcuts import redirect
 
 def home(request):
@@ -15,4 +8,3 @@ def home(request):
         if token is not None and FuerzasMilitares.objects.filter(token_acceso = token).exists():
             token = token[::-1]
     return render(request, "home.html", {"token": token})
->>>>>>> f01ceb22af857cfca7d28c500f7358420e23879e
