2522f6b57a7c5b113e5b4ef6afc2eb97d4dde4e7
ErickBCruz <erickbaezcruz@gmail.com>
2022-09-14 12:13:34 -0500

    Primera version del login

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("landing.urls")),
+    path("login/", include("user.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
