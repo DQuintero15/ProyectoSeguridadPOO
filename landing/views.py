32893ee9ccc61dfc6e9c3715e596ef05af431d1b
Daniel Quintero <quinterod164@gmail.com>
2022-09-14 13:09:36 -0500

    Implementación de verificación por tokens en fuerzas militares

--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from FuerzasMilitares.models import FuerzasMilitares
+from django.shortcuts import redirect
 
-
-def home(request):  
-    return render(request, "home.html")
-
+def home(request):
+    if request.method == "GET":
+        token = request.GET.get("token")
+        if token is not None and FuerzasMilitares.objects.filter(token_acceso = token).exists():
+            token = token[::-1]
+    return render(request, "home.html", {"token": token})
