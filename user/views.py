1ea188b90bbcff6df35d1c3eed858fef8e08f5ce
Daniel Quintero <quinterod164@gmail.com>
2022-10-03 22:59:29 -0500

    Cambios en views de login

--- user/views.py
+++ user/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as login_user, logout as logout_user
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def login_view(request):
+    if request.method == "GET" and request.user.is_authenticated:
+        return redirect("/cerberus/")
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login_user(request, user)
-            return redirect("/cerberus/")
+            return redirect("/cerberus/", {})
         else:
             messages.error(request, "Nombre de usuario o contraseña no válidos")
             return redirect("/accounts/login/")
     return render(request, "login.html", {})
 
 
+@login_required(login_url="/accounts/login/")
 def logout(request):
     logout_user(request)
-    return redirect("/accounts/login")
+    return redirect("/accounts/login", {})
