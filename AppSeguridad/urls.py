76353af7497a30566731026158fa0624457f0bf1
Daniel Quintero <quinterod164@gmail.com>
2022-10-03 23:05:11 -0500

    Urls de personal de batallon, armamento y esquema

--- AppSeguridad/urls.py
+++ AppSeguridad/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.cerberus_home),
-    path("profile/", views.profile)
+    path("", views.cerberus_home),
+    path("profile/", views.profile),
+    path("esquema/", views.esquema_proteccion_view),
+    path("personal/", views.personal),
+    path("armamento/", views.armamento),
 ]
