180e75fb30664cc9c702e53a2b4de4a97c14d137
Daniel Quintero <quinterod164@gmail.com>
2022-10-13 22:54:31 -0500

    Cambios en modelos

--- FuerzasMilitares/models/Poligono.py
+++ FuerzasMilitares/models/Poligono.py
@@ -1,17 +1,12 @@
 from django.db import models
 from django.forms import ModelForm
 from FuerzasMilitares.models.Arma import Arma
+from .PracticaPoligono import PracticaPoligono
+from django import forms
 
 
 class Poligono(models.Model):
-    id_poligono = models.AutoField(primary_key=True)
-    distancia = models.DecimalField(max_digits=10, decimal_places=6)
-    imagen_objetivo = models.ImageField(
-        upload_to="images\\poligonos", null=True, blank=True
-    )
-    provedores = models.IntegerField()
-    cartuchos = models.IntegerField()
-    fecha = models.DateField()
+    id_poligono = models.AutoField(primary_key=True, editable=False, default=0)
     arma = models.ForeignKey(
         Arma,
         null=True,
@@ -19,13 +14,42 @@ class Poligono(models.Model):
         related_name="poligono_arma",
         on_delete=models.CASCADE,
     )
+    provedores = models.PositiveSmallIntegerField()
+    cartuchos = models.PositiveSmallIntegerField()
+    distancia = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    imagen_objetivo = models.ImageField(
+        upload_to="images\\poligonos", null=True, blank=True
+    )
+    practica_poligono = models.ForeignKey(
+        PracticaPoligono,
+        null=True,
+        blank=True,
+        related_name="poligono_practica",
+        on_delete=models.CASCADE,
+        limit_choices_to={"disponible": True},
+    )
 
     class Meta:
         verbose_name = "Poligono"
         verbose_name_plural = "Poligonos"
         db_table = "poligono"
 
+    def __str__(self) -> int:
+        return f"Poligono #{self.id_poligono}"
+
 
 class PoligoForm(ModelForm):
-    model = Poligono
-    field = ("distancia", "imagen_objetivo", "fecha")
+    class Meta:
+        model = Poligono
+        fields = [
+            "practica_poligono",
+            "arma",
+            "distancia",
+            "provedores",
+            "cartuchos",
+            "imagen_objetivo",
+        ]
+        widgets = {"distancia": forms.NumberInput()}
--- FuerzasMilitares/models/PracticaPoligono.py
+++ FuerzasMilitares/models/PracticaPoligono.py
@@ -1,25 +1,13 @@
 from django.db import models
 from FuerzasMilitares.models.InstalacionMilitar import InstalacionMilitar
-from .Poligono import Poligono
-from .Militar import Militar
+from django.forms import ModelForm
+from django import forms
+from FuerzasMilitares.models.Militar import Militar
 
 
 class PracticaPoligono(models.Model):
     id_practica_poligono = models.AutoField(primary_key=True)
-    poligono = models.ForeignKey(
-        Poligono,
-        null=True,
-        blank=True,
-        related_name="poligono_practica",
-        on_delete=models.CASCADE,
-    )
-    militar = models.ForeignKey(
-        Militar,
-        null=True,
-        blank=True,
-        related_name="militar_practica",
-        on_delete=models.CASCADE,
-    )
+    fecha = models.DateField(null=True)
     instalacion = models.ForeignKey(
         InstalacionMilitar,
         null=True,
@@ -27,8 +15,38 @@ class PracticaPoligono(models.Model):
         related_name="practica_instalacion",
         on_delete=models.CASCADE,
     )
+    disponible = models.BooleanField(default=True)
+    modelo_objetivo = models.ImageField(
+        upload_to="images\\modelos", null=True, blank=True
+    )
+    militar = models.ForeignKey(
+        Militar,
+        null=True,
+        blank=True,
+        related_name="practica_militar",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"Practica #{self.id_practica_poligono} de {self.fecha} por {self.militar.rango.abreviatura} {self.militar.nombres[0]}. {self.militar.apellidos}"
 
     class Meta:
         verbose_name = "Practica de Poligono"
         verbose_name_plural = "Practicas de Poligono"
         db_table = "practica_poligono"
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class PracticaPoligoForm(ModelForm):
+    class Meta:
+        model = PracticaPoligono
+        fields = [
+            "fecha",
+            "instalacion",
+            "disponible",
+            "modelo_objetivo",
+        ]
+        widgets = {"fecha": DateInput}
