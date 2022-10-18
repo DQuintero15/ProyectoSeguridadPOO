ac2bcf25115591762f5dc268fbb9bc7f5530a8e2
Daniel Quintero <quinterod164@gmail.com>
2022-10-17 22:42:39 -0500

    Context proccesor de personal en el batallon

--- AppSeguridad/context_processors.py
+++ AppSeguridad/context_processors.py
@@ -1,6 +1,6 @@
 from FuerzasMilitares.models.Militar import Militar
 from FuerzasMilitares.models.UbicacionMilitar import UbicacionMilitar
-from FuerzasMilitares.models.RangoMilitar import RangoMilitar
+from FuerzasMilitares.models.InstalacionMilitar import InstalacionMilitar
 
 
 def getDatosGenerales(request) -> dict:
@@ -14,5 +14,24 @@ def getDatosGenerales(request) -> dict:
             militar_id=id_militar
         )
         context = {"ubicacion": ubicacion, "datos_militar": datos}
+        return context
+    return {}
+
+
+def getPersonalBatallon(request) -> dict:
+    user = request.user
+    if user.is_authenticated:
+        user_id = request.user.id
+        id_militar = Militar.objects.get(usuario_id=user_id).id_militar
+        id_instalacion = InstalacionMilitar.objects.get(
+            id_instalacion=id_militar
+        ).id_instalacion
+
+        militares = (
+            UbicacionMilitar.objects.select_related("militar")
+            .select_related("instalacion")
+            .filter(instalacion_id=id_instalacion)
+        )
+        context = {"militares": militares}
         return context
     return {}
