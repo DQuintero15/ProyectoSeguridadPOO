61f8201dcf4f6397777d9feb4a3939eb6e4733a4
Daniel Quintero <quinterod164@gmail.com>
2022-11-10 22:17:57 -0500

    Cambios en rutas de archivos

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "template")],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
